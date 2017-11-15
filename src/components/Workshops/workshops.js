a7e94c79685ada16a561105814627959b4e09c6a
mario diaz <mdiaz00147@gmail.com>
2017-11-15 15:42:33 -0500

    first components

---
+++ src/components/Workshops/workshops.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import {
+  Route,
+  HashRouter
+} from "react-router-dom";
+import Menu from './../../navigation/headerMenu'
+
+
+class Workshops extends Component {
+  render() {
+    return (
+      <div>
+        <div className="wrapper">
+
+        <Menu/>
+
+         <div id="hero" className="carousel slide carousel-fade" data-ride="carousel">
+
+
+      
+
+        </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Workshops;
